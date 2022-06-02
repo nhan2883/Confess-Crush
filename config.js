f6b853b01f3b959f1aab59295ad7cc07cb861f97
nhan2883 <106746117+nhan2883@users.noreply.github.com>
2022-06-02 19:58:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "câu hỏi dành cho cậu",
+    introTitle: 'vân 🐴 đ ĩ',
+    introDesc: `sau 1 thời gian quen biết thì mình quyết định gửi tới cậu câu hỏi nhanh này`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'có phải hay không rằng mình rất xinh đẹp 🥰',
+    desc: 'Phải trả lời thật lòng đó nhaaaaa ',
+    btnYes: 'xinh lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'mình biết cậu mến mình mà đúng không nào',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'đó biết ngay mà',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messDesc: 'vậy thi nhớ gửi bồ câu đưa thư cho mình nhaaaaa',
+    btnAccept: 'Okiiiii luôn nà',
+    messlink: https://www.facebook.com/profile.php?id=100052589133676
 }
